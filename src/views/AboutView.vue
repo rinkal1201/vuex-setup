eb80aba49f4fe37cef7642b8945a925f9976870e
rinkal1201 <rinkal.kevadiya25@exhibytesolution.com>
2022-12-07 18:09:30 +0530

    # Directives
    -> Implement custom directive.

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,81 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div class="about">
+    <form @submit.prevent="onUserSubmit" style="margin-top:40px">
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Enter name" v-model="name">
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" placeholder="Enter email" v-model="email">
+        </div>
+        <button type="submit" class="btn btn-primary" style="height:35px;width:100px">Add</button>
+    </form>
+
+    <div class="container table-responsive py-5">
+        <table class="table table-bordered table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">User Name</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Twubric Score </th>
+                    <th scope="col">Friends</th>
+                    <th scope="col">Influence</th>
+                    <th scope="col">Chirpiness</th>
+                    <th scope="col">Join Date</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(i, key) in testsList" :key='key'>
+                    <td>{{i.uid}}</td>
+                    <td>{{i.username}}</td>
+                    <td><img :src="i.image" height="30" width="30" /></td>
+                    <td>{{i.twubric.total}}</td>
+                    <td>{{i.twubric.friends}}</td>
+                    <td>{{i.twubric.influence}}</td>
+                    <td>{{i.twubric.chirpiness}}</td>
+                    <td>{{i.join_date}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
 </template>
+
+<script>
+import {
+    mapActions,
+    mapGetters
+} from "vuex";
+import store from "../store";
+
+export default {
+    name: 'AboutView',
+    data() {
+        return {
+            name: '',
+            email: ''
+        }
+    },
+
+    methods: {
+        ...mapActions(["addUsers"]),
+        ...mapActions(["fetchTests"]),
+
+        onUserSubmit() {
+            this.addUsers({               // For add user data.
+                name: this.name,
+                email: this.email
+            })
+        },
+    },
+    computed: mapGetters(["testsList"]),
+
+    mounted() {
+        store.dispatch("fetchTests");    // For get data.
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/directive/index.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <div>
+        <label v-fontSize>Hover On ME</label>
+        <h1 v-color="'red'">
+            Good Morning</h1>
+
+        <h2 v-color="'yellow'">
+            Hello</h2>
+
+        <p v-color="'green'">
+            Hii</p>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "Index",
+
+    directives: {
+        // Directives for font size.
+        fontSize: {
+            bind(el, binding) {
+                el.onmouseover = function () {
+                    el.style.fontSize = "50px";
+                };
+                el.onmouseout = function () {
+                    el.style.fontSize = "15px";
+                };
+            }
+        },
+
+        // Directives for dynamic color.
+        color: {
+            bind(el, binding) {
+                el.style.color = binding.value;
+            }
+        }
+    },
+
+};
+</script>
+
+<style scoped>
+    </style>
